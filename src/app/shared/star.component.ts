ec8ef3f80860f9aa8285cc61e4dbb3af2db5ccde
mohamed-faalil <mohamedfaalil05@gmail.com>
2024-01-21 20:48:25 +0530

    01) passed the data to nested component. 02) once input property is got changed, used ngOnChange hook and manipulated cropWidth property

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,24 +1,17 @@
-import {Component, OnChanges, OnInit, SimpleChanges} from "@angular/core";
+import {Component, Input, OnChanges, SimpleChanges} from "@angular/core";
 
 @Component({
   selector: 'pm-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnChanges, OnInit{
+export class StarComponent implements OnChanges{
   readonly totalWidth: number = 75;
   readonly totalRating: number = 5;
-  rating:number = 4;
+  @Input() rating:number = 0;
   cropWidth:number = 0;
 
   ngOnChanges(changes: SimpleChanges): void {
     this.cropWidth = (this.totalWidth/this.totalRating) *  this.rating;
   }
-
-  ngOnInit(): void {
-    this.cropWidth = this.totalWidth;
-  }
-
-
-
 }
